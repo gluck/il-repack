7c363b03f3f3d919b8cdc6d4774482058c0f727a
Francois Valdy <francois.valdy@gmail.com>
2015-04-27 23:58:03 +0200

    Moar packages

--- ILRepack.Tests/RepackNuGetTests.cs
+++ ILRepack.Tests/RepackNuGetTests.cs
@@ -15,22 +15,34 @@ namespace ILRepack.Tests
 {
     class RepackNuGetTests
     {
-        [TestCase("Rx_Experimental-Main", "1.1.11111")]
-        [TestCase("Rx-Core", "2.2.5")]
+        [TestCase("Antlr", "3.5.0.2")]
+        [TestCase("Autofac", "3.5.2")]
+        [TestCase("AutoMapper", "3.3.1")]
+        [TestCase("Castle.Core", "3.3.3")]
+        [TestCase("Dapper", "1.40.0")]
+        // Missing refs [TestCase("EntityFramework", "6.1.3")]
         // FIXME [TestCase("FSharp.Core", "3.1.2.1")]
+        [TestCase("Iesi.Collections", "4.0.1.4000")]
+        [TestCase("MahApps.Metro", "1.1.2")]
+        [TestCase("Microsoft.Bcl", "1.1.10")]
+        [TestCase("Microsoft.TeamFoundation.Common", "12.0.21005.1")]
+        [TestCase("Newtonsoft.Json", "6.0.8")]
+        [TestCase("NHibernate", "4.0.3.4000")]
+        [TestCase("Ninject", "3.2.2")]
+        // PCL [TestCase("Remotion.Linq", "1.15.15")]
+        [TestCase("RestSharp", "105.0.1")]
+        [TestCase("Rx-Core", "2.2.5")]
+        [TestCase("Rx_Experimental-Main", "1.1.11111")]
+        [TestCase("SharpZipLib", "0.86.0")]
+        [TestCase("System.Spatial", "5.6.4")]
         public void RoundtripNupkg(string package, string version)
         {
             var count = NuGetHelpers.GetNupkgContentAsync(package, version)
             .Where(t => Path.GetExtension(t.Item1) == ".dll")
-            .Where(t => new [] { @"lib\net35", @"lib\net40", @"lib\net4" } .Contains(Path.GetDirectoryName(t.Item1).ToLower()))
-            .Do(t => {
-                using (var stream = t.Item2()) {
-                    using (var file = new FileStream("foo.dll", FileMode.Create)) {
-                        stream.CopyTo(file);
-                    }
-                }
-            }).Do(file => {
-                Console.WriteLine("Merging {0}", file);
+            .Where(t => new[] { @"lib", @"lib\20", @"lib\net20", @"lib\net35", @"lib\net40", @"lib\net4", @"lib\net45" }.Contains(Path.GetDirectoryName(t.Item1).ToLower()))
+            .Do(t => SaveAs(t.Item2(), "foo.dll"))
+            .Do(file => {
+                Console.WriteLine("Merging {0}", file.Item1);
                 ICommandLine commandLine = new CommandLine(new []{"/out:test.dll","foo.dll","/log"});
                 ILogger logger = new RepackLogger();
                 RepackOptions options = new RepackOptions(commandLine, logger, new FileWrapper());
@@ -40,6 +52,18 @@ namespace ILRepack.Tests
                 Assert.IsTrue(File.Exists("test.dll"));
             }).ToEnumerable().Count();
             Assert.IsTrue(count > 0);
+        }
+
+        private static void SaveAs(Stream input, string fileName)
+        {
+            var path = Path.GetDirectoryName(fileName);
+            if (!string.IsNullOrEmpty(path) && !Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            using (var stream = input) {
+                using (var file = new FileStream(fileName, FileMode.Create)) {
+                    stream.CopyTo(file);
+                }
+            }
         }
     }
 }
