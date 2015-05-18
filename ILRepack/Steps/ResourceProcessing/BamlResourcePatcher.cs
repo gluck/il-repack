471689cb2b2cc376a289a0518f29f50f46273991
Timotei Dolean <timotei21@gmail.com>
2015-05-18 15:34:24 +0300

    Merge BamlResourceProcessor into the patcher

--- ILRepack/Steps/ResourceProcessing/BamlResourcePatcher.cs
+++ ILRepack/Steps/ResourceProcessing/BamlResourcePatcher.cs
@@ -13,18 +13,33 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+using Confuser.Renamer.BAML;
 using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Resources;
 
 namespace ILRepacking.Steps.ResourceProcessing
 {
     internal class BamlResourcePatcher : IResProcessor
     {
-        private readonly IRepackContext _repackContext;
+        private readonly AssemblyDefinition _mainAssembly;
+        private readonly List<AssemblyDefinition> _mergedAssemblies;
+
+        private readonly Dictionary<Type, Action<BamlRecord>> _nodeProcessors;
 
         public BamlResourcePatcher(IRepackContext repackContext)
         {
-            _repackContext = repackContext;
+            _mainAssembly = repackContext.PrimaryAssemblyDefinition;
+            _mergedAssemblies = repackContext.MergedAssemblies;
+
+            _nodeProcessors = new Dictionary<Type, Action<BamlRecord>>
+            {
+                { typeof(AssemblyInfoRecord), r => ProcessRecord((AssemblyInfoRecord)r) },
+                { typeof(PropertyWithConverterRecord), r => ProcessRecord((PropertyWithConverterRecord)r) },
+                { typeof(XmlnsPropertyRecord), r => ProcessRecord((XmlnsPropertyRecord)r) }
+            };
         }
 
         public bool Process(
@@ -33,13 +48,56 @@ namespace ILRepacking.Steps.ResourceProcessing
             if (!resource.IsBamlStream)
                 return false;
 
-            var bamlResourceProcessor = new BamlResourceProcessor(_repackContext.PrimaryAssemblyDefinition,
-                _repackContext.MergedAssemblies,
-                resource);
-
-            resource.data = bamlResourceProcessor.GetProcessedResource();
+            resource.data = GetProcessedResource(resource);
 
             return false;
+        }
+
+        private byte[] GetProcessedResource(Res resource)
+        {
+            BamlDocument bamlDocument = BamlUtils.FromResourceBytes(resource.data);
+
+            foreach (BamlRecord node in bamlDocument)
+            {
+                Action<BamlRecord> recordProcessor;
+
+                if (_nodeProcessors.TryGetValue(node.GetType(), out recordProcessor))
+                {
+                    recordProcessor(node);
+                }
+            }
+
+            //TODO: diminishing return optimisation: remove duplications + update assembly ids
+
+            return BamlUtils.ToResourceBytes(bamlDocument);
+        }
+
+        private void ProcessRecord(PropertyWithConverterRecord record)
+        {
+            record.Value = XamlResourcePathPatcherStep.PatchPath(record.Value, _mainAssembly, _mergedAssemblies);
+        }
+
+        private void ProcessRecord(AssemblyInfoRecord record)
+        {
+            var assemblyDefinition = _mergedAssemblies.FirstOrDefault(
+                asm => asm.Name.Name == record.AssemblyFullName || asm.Name.FullName == record.AssemblyFullName);
+
+            if (assemblyDefinition != null)
+            {
+                record.AssemblyFullName = _mainAssembly.Name.Name;
+            }
+        }
+
+        private void ProcessRecord(XmlnsPropertyRecord record)
+        {
+            string xmlNamespace = record.XmlNamespace;
+            const string AssemblyDef = "assembly=";
+            int assemblyStart = xmlNamespace.IndexOf(AssemblyDef, StringComparison.Ordinal);
+            if (assemblyStart == -1)
+                return;
+
+            string xmlNsWithoutAssembly = xmlNamespace.Substring(0, assemblyStart);
+            record.XmlNamespace = string.Format("{0}{1}{2}", xmlNsWithoutAssembly, AssemblyDef, _mainAssembly.Name.Name);
         }
     }
 }
