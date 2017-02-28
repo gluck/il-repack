1c9116a272fad1f526fa730165ff58e347b061e8
Timotei Dolean <timotei21@gmail.com>
2017-02-28 17:22:59 +0200

    Add the forwarding usecase for System.Runtime

---
+++ ILRepack.IntegrationTests/Scenarios/DotNet462Application/AssemblyMetadataAttribute.cs
@@ -0,0 +1,11 @@
+﻿// Force this to be in the same namespace as System.Runtime, since this seem to trigger an issue because of the forwarded type vs defined type
+namespace System.Reflection
+{
+    [AttributeUsage(AttributeTargets.All)]
+    internal class AssemblyMetadataAttribute : Attribute
+    {
+        public AssemblyMetadataAttribute(string key, string value)
+        {
+        }
+    }
+}
