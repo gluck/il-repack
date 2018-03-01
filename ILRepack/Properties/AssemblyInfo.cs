29a379335e463a7bf4fbec50482c9209a12c2d35
WMJ <wmj@live.cn>
2018-03-01 18:21:14 +0800

    Allow to use RepackDropAttribute to exclude members from merging.

--- ILRepack/Properties/AssemblyInfo.cs
+++ ILRepack/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.14.0")]
-[assembly: AssemblyFileVersion("2.0.14.0")]
+[assembly: AssemblyFileVersion("2.0.14.3")]
 
 [assembly: InternalsVisibleTo("ILRepack.Tests")]
 [assembly: InternalsVisibleTo("ILRepack.IntegrationTests")]
