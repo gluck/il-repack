82cc690f7efdb6768c61f25ba5d10649faee58b7
Igor Kiselev <igor.kiselev@appdynamics.com>
2021-01-06 20:06:33 -0800

    Assembly strong name Linux compatibility

---
+++ ILRepack.Tests/Steps/SigningStepTest.cs
@@ -0,0 +1,15 @@
+﻿using ILRepacking.Steps;
+using ILRepacking.Steps.SourceServerData;
+using NUnit.Framework;
+
+namespace ILRepack.Tests.Steps
+{
+    internal class SigningStepTest
+    {
+        [TestCase]
+        public static void Validate_GetPublicKey_Resolved()
+        {
+            Assert.NotNull(SigningStep.GetPublicKey);
+        }
+    }
+}
