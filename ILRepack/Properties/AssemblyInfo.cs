45174837175d9aea6c5f7bef62d19356df5ac56a
Peter Ng <peter.ng@itiviti.com>
2021-02-19 11:11:23 +0800

    Update gradle builds
    Change-Id: I81c2745afc61f7fd29dc34e88ea0b463f33c3238

--- ILRepack/Properties/AssemblyInfo.cs
+++ ILRepack/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ILRepack")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ILRepack")]
-[assembly: AssemblyCopyright("Copyright © Francois Valdy 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -21,19 +9,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b140d8b2-68f8-4324-965b-5c047c3745ab")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.18.0")]
-[assembly: AssemblyFileVersion("2.0.18.0")]
 
 [assembly: InternalsVisibleTo("ILRepack.Tests")]
 [assembly: InternalsVisibleTo("ILRepack.IntegrationTests")]
