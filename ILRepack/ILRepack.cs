66cdd54efb14b5ea19cd5af373f7b224d6448f65
Kirill Osenkov <github@osenkov.com>
2023-12-22 18:59:10 -0800

    Don't copy permissions for libraries

--- ILRepack/ILRepack.cs
+++ ILRepack/ILRepack.cs
@@ -357,7 +357,7 @@ namespace ILRepacking
                 Logger.Info("Writing output assembly to disk");
                 // If this is an executable and we are on linux/osx we should copy file permissions from
                 // the primary assembly
-                if (isUnixEnvironment)
+                if (isUnixEnvironment && (kind == ModuleKind.Console || kind == ModuleKind.Windows))
                 {
                     Stat stat;
                     Logger.Info("Copying permissions from " + PrimaryAssemblyFile);
