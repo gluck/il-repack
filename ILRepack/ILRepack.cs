662df901a3eca718dc4f89d7afa3755ee81553ca
Kirill Osenkov <github@osenkov.com>
2024-01-05 18:26:27 -0800

    Add a step to merge SourceLink debug informations
    Add a TinyJson.JSONParser from https://github.com/zanders3/json
    This avoids adding a dependency on Newtonsoft.Json just for source link

--- ILRepack/ILRepack.cs
+++ ILRepack/ILRepack.cs
@@ -398,6 +398,7 @@ namespace ILRepacking
                     new ReferencesFixStep(Logger, this, _repackImporter, Options),
                     new PublicTypesFixStep(Logger, this),
                     new XamlResourcePathPatcherStep(Logger, this),
+                    new SourceLinkStep(Logger, this),
                     sourceServerDataStep
                 };
 
---
+++ ILRepack/Steps/SourceLinkStep.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Mono.Cecil.Cil;
+
+namespace ILRepacking.Steps
+{
+    internal class SourceLinkStep : IRepackStep
+    {
+        private readonly ILogger _logger;
+        private readonly IRepackContext _repackContext;
+
+        public SourceLinkStep(ILogger logger, IRepackContext repackContext)
+        {
+            _logger = logger;
+            _repackContext = repackContext;
+        }
+
+        private class SourceLinkData
+        {
+            public Dictionary<string, string> documents;
+        }
+
+        public void Perform()
+        {
+            var aggregated = new Dictionary<string, string>();
+
+            foreach (var input in _repackContext.MergedAssemblies)
+            {
+                var module = input.MainModule;
+                if (!module.HasCustomDebugInformations)
+                {
+                    continue;
+                }
+
+                foreach (var custom in module.CustomDebugInformations)
+                {
+                    if (custom is not SourceLinkDebugInformation sourceLinkDebugInformation)
+                    {
+                        continue;
+                    }
+
+                    var sourceLink = sourceLinkDebugInformation.Content;
+                    if (string.IsNullOrWhiteSpace(sourceLink))
+                    {
+                        continue;
+                    }
+
+                    var data = TinyJson.JSONParser.FromJson<SourceLinkData>(sourceLink);
+                    if (data == null || data.documents == null)
+                    {
+                        continue;
+                    }
+
+                    foreach (var kvp in data.documents)
+                    {
+                        aggregated[kvp.Key] = kvp.Value;
+                    }
+                }
+            }
+
+            if (aggregated.Count == 0)
+            {
+                return;
+            }
+
+            string finalJson = WriteJson(aggregated);
+            var sourceLinkInformation = new SourceLinkDebugInformation(finalJson);
+
+            _repackContext.TargetAssemblyMainModule.CustomDebugInformations.Add(sourceLinkInformation);
+
+            _logger.Verbose($"Wrote aggregated SourceLink json with {aggregated.Count} entries");
+        }
+
+        private string WriteJson(Dictionary<string, string> dictionary)
+        {
+            var sb = new StringBuilder();
+            sb.Append("{\"documents\":{");
+
+            bool isFirst = true;
+
+            foreach (var kvp in dictionary)
+            {
+                if (isFirst)
+                {
+                    isFirst = false;
+                }
+                else
+                {
+                    sb.Append(",");
+                }
+
+                sb.Append("\"");
+                sb.Append(kvp.Key);
+                sb.Append("\"");
+                sb.Append(":");
+                sb.Append("\"");
+                sb.Append(kvp.Value);
+                sb.Append("\"");
+            }
+
+            sb.Append("}}");
+
+            var result = sb.ToString();
+            return result;
+        }
+    }
+}
