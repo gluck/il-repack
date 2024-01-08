92843eace06c4eff3d6296a6fc4470ba725c57aa
Kirill Osenkov <github@osenkov.com>
2024-01-07 20:05:39 -0800

    Document more command line arguments

--- ILRepack/Application.cs
+++ ILRepack/Application.cs
@@ -77,32 +77,43 @@ Syntax: ILRepack.exe [Options] /out:<path> <path_to_primary> [<other_assemblies>
  - <other_assemblies> other assemblies to merge with the primary one
  - /wildcards         allows (and resolves) file wildcards (e.g. *.dll) in input assemblies
 
- - /lib:<path>        path(s) to search directories to resolve referenced assemblies (can be specified multiple times)
+ - /lib:<path>        path(s) to search directories to resolve referenced assemblies 
+                      (can be specified multiple times).
+                      If you get 'unable to resolve assembly' errors specify a path to a directory
+                      where the assembly can be found.
 
- - /target:kind       target assembly kind (library, exe, winexe supported, default is same as primary assembly)
+ - /target:kind       target assembly kind [library|exe|winexe], default is same as primary assembly
  - /ver:M.X.Y.Z       target assembly version
  - /keyfile:<path>    keyfile to sign the output assembly
+ - /keycontainer:<c>  key container
  - /delaysign         set the key, but don't sign the assembly
 
  - /internalize       make all types except in the first assembly 'internal'.
                       Types in the transitive closure of public API remain public.
- - /union             merges types with identical names into one
+ - /internalize:<exclude_file>
+                      Each line is either a regex/ full type name not to internalize
+                      or an assembly name not to internalize (.dll extension optional)
+ - /renameinternalized
+                      rename each internalized type to a new unique name
  - /allowdup:Type     keep duplicates of the specified type, may be specified more than once
  - /allowdup          if no other /allowdup arguments specified, allow all duplicate types
+ - /union             merges types with identical names into one
  - /repackdrop:RepackDropAttribute 
                       allows dropping members denoted by this attribute name when merging
+ - /allowduplicateresources 
+                      allows to duplicate resources in output assembly (by default they're ignored)
+ - /noRepackRes       do not add the resource '{ResourcesRepackStep.ILRepackListResourceName}' with all merged assembly names
 
  - /copyattrs         copy assembly attributes (by default only the primary assembly attributes are copied)
  - /attr:<path>       take assembly attributes from the given assembly file
  - /allowMultiple     when copyattrs is specified, allows multiple attributes (if type allows)
  - /targetplatform:P  specify target platform (v1, v1.1, v2, v4 supported)
+ - /keepotherversionreferences
+                      take reference assembly version into account when removing references
 
  - /skipconfig        skips merging config files
  - /illink            merge IL Linker files
  - /xmldocs           merges XML documentation as well
-
- - /allowduplicateresources allows to duplicate resources in output assembly (by default they're ignored)
- - /noRepackRes       do not add the resource '{ResourcesRepackStep.ILRepackListResourceName}' with all merged assembly names
  - /ndebug            disables symbol file generation (omit this if you want symbols and debug information)
  - /zeropekind        allows assemblies with Zero PeKind (but obviously only IL will get merged)
  - /index             stores file:line debug information as type/method attributes (requires PDB)
