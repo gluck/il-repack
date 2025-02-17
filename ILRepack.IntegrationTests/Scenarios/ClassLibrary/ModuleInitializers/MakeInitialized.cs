50c27c2a349bb5fdd67bfccb9e4d3230dd2a3a19
warappa <spoiltnr1@gmx.at>
2025-02-17 02:51:19 +0100

    Amend ClassLibrary, AnotherClassLibrary and NestedLibraryUsageInXAML to also test for right execution of module initializers

---
+++ ILRepack.IntegrationTests/Scenarios/ClassLibrary/ModuleInitializers/MakeInitialized.cs
@@ -0,0 +1,32 @@
+﻿//
+// Copyright (c) 2025 David Rettenbacher
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+namespace ClassLibrary.ModuleInitializers
+{
+    public class MakeInitialized
+    {
+        public static bool IsInitialized { get; private set; }
+
+        public static int Counter { get; private set; }
+
+        public static void Initialize()
+        {
+            Counter++;
+
+            IsInitialized = true;
+        }
+    }
+}
--- ILRepack.IntegrationTests/Scenarios/NestedLibraryUsageInXAML/MainWindow.xaml.cs
+++ ILRepack.IntegrationTests/Scenarios/NestedLibraryUsageInXAML/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using AnotherClassLibrary;
 
 namespace NestedLibraryUsageInXAML
@@ -13,6 +14,18 @@ namespace NestedLibraryUsageInXAML
         private async void MainWindowLoaded(object sender, RoutedEventArgs e)
         {
             await new BclAsyncUsage().GetNumber();
+
+            if (!ClassLibrary.ModuleInitializers.MakeInitialized.IsInitialized &&
+                ClassLibrary.ModuleInitializers.MakeInitialized.Counter == 1)
+                throw new InvalidOperationException($"ModuleInitializer of '{nameof(ClassLibrary.ModuleInitializers.LibraryModuleInitializer)}' was not executed");
+
+            if (!AnotherClassLibrary.ModuleInitializers.MakeInitialized.IsInitialized &&
+                AnotherClassLibrary.ModuleInitializers.MakeInitialized.Counter == 2)
+                throw new InvalidOperationException($"ModuleInitializer of '{nameof(AnotherClassLibrary.ModuleInitializers.LibraryModuleInitializer)}' was not executed or not in right order");
+
+            if (Program.Counter != 3)
+                throw new InvalidOperationException($"ModuleInitializers of '{nameof(Program)}' were not executed or not in right order");
+
             Close();
         }
     }
